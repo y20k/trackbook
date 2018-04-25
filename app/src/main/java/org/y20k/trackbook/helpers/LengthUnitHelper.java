69d902eb4975571301b7ec65b99d4322da374ec2
y20k <mail@y20k.org>
2018-04-25 22:58:59 +0200

    second try: moved metric/imperial conversion to its own helper class

---
+++ app/src/main/java/org/y20k/trackbook/helpers/LengthUnitHelper.java
@@ -0,0 +1,77 @@
+/**
+ * LengthUnitHelper.java
+ * Implements the LengthUnitHelper class
+ * A LengthUnitHelper offers helper methods for dealing with unit systems and locales
+ *
+ * This file is part of
+ * TRACKBOOK - Movement Recorder for Android
+ *
+ * Copyright (c) 2016-18 - Y20K.org
+ * Licensed under the MIT-License
+ * http://opensource.org/licenses/MIT
+ *
+ * Trackbook uses osmdroid - OpenStreetMap-Tools for Android
+ * https://github.com/osmdroid/osmdroid
+ */
+
+package org.y20k.trackbook.helpers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+
+/**
+ * LengthUnitHelper class
+ */
+public final class LengthUnitHelper implements TrackbookKeys {
+
+
+    /* Converts for the default locale a distance value to a readable string */
+    public static String convertDistanceToString(double distance) {
+        return convertDistanceToString(distance, getUnitSystem(Locale.getDefault()));
+    }
+
+
+    /* Converts for the given uni System a distance value to a readable string */
+    public static String convertDistanceToString(double distance, int unitSystem) {
+        // check for locale and set unit system accordingly
+        String unit;
+        if (unitSystem == IMPERIAL) {
+            // convert distance to feet
+            distance = distance * 3.28084f;
+            // set measurement unit
+            unit = "ft";
+        } else {
+            // set measurement unit
+            unit = "m";
+        }
+        return String.format (Locale.ENGLISH, "%.0f", distance) + unit;
+    }
+
+
+    /* Determines which unit system the device is using (metric or imperial) */
+    private static int getUnitSystem(Locale locale) {
+        // America (US), Liberia (LR), Myanmar(MM) use the imperial system
+        List<String> imperialSystemCountries = Arrays.asList("US", "LR", "MM");
+        String countryCode = locale.getCountry();
+
+        if (imperialSystemCountries.contains(countryCode)){
+            return IMPERIAL;
+        } else {
+            return METRIC;
+        }
+    }
+
+
+    /* Returns the opposite unit system based on the current locale */
+    public static int getOppositeUnitSystem() {
+        int unitSystem = getUnitSystem(Locale.getDefault());
+        if (unitSystem == METRIC){
+            return IMPERIAL;
+        } else {
+            return METRIC;
+        }
+    }
+
+}
